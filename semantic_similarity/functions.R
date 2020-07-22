51813b1b9a7545d62e96e954c7d55916fe1c34ec
Ted Natoli <ted.e.natoli@gmail.com>
2020-07-22 08:33:02 -0400

    initializing repo

---
+++ semantic_similarity/functions.R
@@ -0,0 +1,44 @@
+# Supporting functions for semantic similarity analyses
+
+# compute the jaccard index between vectors x and y
+jaccard <- function(x, y) {
+    length(intersect(x, y)) / length(union(x, y))
+}
+
+# define a function to compute similarity between a disease gene list
+# and a collection of drug gene lists
+# this will simply wrap the clusterSim function from GOSemSim
+# also enable support for simple jaccard index, which we can use to compare
+# with semantic similarity metrics
+disease_drug_sim <- function(disease_gene_list, drug_gene_lists, measure,
+                             mc.cores=4, ...) {
+    # use mclapply to run computations in parallel
+    res <- mclapply(drug_gene_lists, function(x) {
+        tryCatch({
+            if (measure=="jaccard") {
+                # just compute jaccard index
+                jaccard(disease_gene_list, x)
+            } else {
+                clusterSim(disease_gene_list, x, measure=measure, ...)
+                # catch any errors if they occur
+            }}, error = function(e) {
+                message(e, " ", x, "\n")
+                return(NA)
+            })
+    }, mc.cores = mc.cores)
+    names(res) <- names(drug_gene_lists)
+    # unlist so res becomes a vector
+    return(unlist(res))
+}
+
+# function to generate null (permuted) gene lists
+make_null_sets <- function(sets, universe=NULL, nperm=1e2) {
+    if (is.null(universe)) {
+        universe <- Reduce(union, sets)
+    }
+    lapply(1:nperm, function(i) {
+        sapply(sets, function(x) {
+            sample(universe, length(x))
+        }, USE.NAMES = T, simplify = F)
+    })
+}
