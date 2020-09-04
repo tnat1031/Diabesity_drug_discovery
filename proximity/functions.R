b934144443b43a8c7a52a8a69940fbf3d9dcc60f
Ted Natoli <ted.e.natoli@gmail.com>
2020-09-04 14:05:12 -0400

    compute and aggregate proximity scores

--- proximity/functions.R
+++ proximity/functions.R
@@ -3,7 +3,7 @@
 proximity <- function(graph, to, from) {
     # use igraph's shortest_paths function to get the shortest paths
     # just take the length of the first one as the proximity
-    sp <- shortest_paths(g, from, to)
+    sp <- shortest_paths(graph, from, to)
     l <- length(sp$vpath[[1]])
     # if there is no path b/w the two, return NaN
     if (l == 0) return(NaN) else return(l - 1)
@@ -23,4 +23,26 @@ filter_gene_lists <- function(x, universe) {
     }, USE.NAMES = T)
     # remove any that now have zero genes
     return(tmp[lengths(tmp) > 0])
+}
+
+# helper function to summarize a proximity matrix
+summarize_prox_mat <- function(prox_mat) {
+    if (is.matrix(prox_mat)) {
+        nr <- nrow(prox_mat)
+        nc <- ncol(prox_mat)
+        mat_size <- nr * nc
+    } else {
+        nr <- NaN
+        nc <- NaN
+        mat_size <- length(prox_mat)
+    }
+    data.table(
+        nr = nr,
+        nc = nc,
+        mat_size = mat_size,
+        min_prox = min(prox_mat, na.rm=T),
+        mean_prox = mean(prox_mat, na.rm=T),
+        med_prox = median(prox_mat, na.rm=T),
+        max_prox = max(prox_mat, na.rm=T)
+    )
 }
